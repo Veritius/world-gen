35697c12da630f7f93ad1d3d48e7338693079590
Veritius <veritiusgaming@gmail.com>
2023-05-18 12:39:36 +1000

    Improve Simulation

--- src/world/sim.rs
+++ src/world/sim.rs
@@ -1,52 +1,45 @@
 use std::{sync::{RwLock, Arc}, thread::{JoinHandle, self}};
 use bevy_ecs::{world::World, schedule::Schedule, system::Resource};
 
-pub enum Simulation {
-    Unstarted,
-    Started {
-        status: Arc<RwLock<SimulationStatus>>,
-        handle: JoinHandle<World>,
-    }
+pub struct Simulation {
+    pub status: Arc<RwLock<SimulationStatus>>,
+    pub handle: JoinHandle<World>,
 }
 
 impl Simulation {
-    pub fn start_simulation(mut self, mut world: World, mut schedule: Schedule) -> Result<Arc<RwLock<SimulationStatus>>, ()> {
-        match self {
-            Simulation::Unstarted => {
-                let status = Arc::new(RwLock::new(SimulationStatus::default()));
+    pub fn new(mut world: World, mut schedule: Schedule) -> Self {
+        let status = Arc::new(RwLock::new(SimulationStatus::default()));
 
-                // Clone so the value isn't moved out when it's needed later
-                let status_for_thread = status.clone();
+        // Clone so the value isn't moved out when it's needed later
+        let status_for_thread = status.clone();
 
-                let thread = thread::spawn(move || {
-                    let status = status_for_thread;
+        let thread = thread::spawn(move || {
+            let status = status_for_thread;
 
-                    loop {
-                        schedule.run(&mut world);
+            loop {
+                schedule.run(&mut world);
 
-                        // Marker resource that stops the execution
-                        if world.get_resource::<SimulationComplete>().is_some() { break; }
-                    }
+                // Marker resource that stops the execution
+                if world.get_resource::<SimulationComplete>().is_some() { break; }
+            }
 
-                    world
-                });
+            world
+        });
 
-                self = Simulation::Started { status: status.clone(), handle: thread };
-                Ok(status)
-            },
-            Simulation::Started { status: _, handle: _ } => { Err(()) },
-        }
+        Simulation { status: status.clone(), handle: thread }
     }
 }
 
 pub struct SimulationStatus {
     pub steps_complete: u32,
+    pub steps_total: u32,
 }
 
 impl Default for SimulationStatus {
     fn default() -> Self {
         Self {
-            steps_complete: 0
+            steps_complete: 0,
+            steps_total: 0,
         }
     }
 }
