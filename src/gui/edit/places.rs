f3e8b348e61dbc8fdcf85b848004250dad353ae7
Veritius <veritiusgaming@gmail.com>
2023-05-23 11:06:31 +1000

    Revert "Graph structure for iteration"
    This reverts commit fe12e100ee575641f8f73a32ffa44794fb1cfc6c.

--- src/gui/edit/places.rs
+++ src/gui/edit/places.rs
@@ -1,9 +1,7 @@
-use std::collections::{BTreeMap, BTreeSet};
-use bevy::{ecs::system::CommandQueue, prelude::{Entity, Parent, Children, With, Without}};
+use std::collections::BTreeMap;
+use bevy::ecs::system::CommandQueue;
 use eframe::egui;
-use either::Either::{Left, self, Right};
-use petgraph::Graph;
-use crate::world::{sim::SimulationData, place::{Settlement, Region}, thing::Name};
+use crate::world::sim::SimulationData;
 
 pub(super) fn edit_places_ui(
     ui: &mut egui::Ui,
@@ -11,63 +9,5 @@ pub(super) fn edit_places_ui(
     queue: &mut CommandQueue,
     sim: &mut SimulationData,
 ) {
-    let world = &mut sim.app.world;
 
-    let mut graph: Graph<Either<(), Entity>, ()> = Graph::new();
-    let root = graph.add_node(Left(()));
-
-    let mut region_query = world.query_filtered::<(Entity, &Region, &Name, Option<&Parent>, Option<&Children>), Without<Settlement>>();
-    let mut settlement_query = world.query_filtered::<(Entity, &Settlement, &Name, Option<&Parent>), Without<Region>>();
-
-    let mut nodes = BTreeMap::new();
-
-    // Add nodes to graph
-    for (entity, _, _, _, _) in region_query.iter(&world) {
-        nodes.insert(entity, graph.add_node(Right(entity)));
-    }
-
-    for (entity, _, _, _) in settlement_query.iter(&world) {
-        nodes.insert(entity, graph.add_node(Right(entity)));
-    }
-
-    // Add edges
-    for (entity, _, _, parent, children) in region_query.iter(&world) {
-        let this = nodes[&entity];
-        // Parent edges
-        if parent.is_some() {
-            let parent = nodes.get(&parent.unwrap());
-            if parent.is_some() {
-                // Child of parent
-                graph.add_edge(*parent.unwrap(), this, ());
-            } else {
-                // Child of root
-                graph.add_edge(root, this, ());
-            }
-        }
-        // Child edges
-        if children.is_some() {
-            let children = children.unwrap();
-            for child in children.iter() {
-                let child = nodes.get(child);
-                if child.is_some() {
-                    // Parent of child
-                    graph.add_edge(this, *child.unwrap(), ());
-                }
-            }
-        }
-    }
-
-    for (entity, _, _, parent) in settlement_query.iter(&world) {
-        let this = nodes[&entity];
-        if parent.is_some() {
-            let parent = nodes.get(&parent.unwrap().get());
-            if parent.is_some() {
-                // Child of parent
-                graph.add_edge(*parent.unwrap(), this, ());
-            } else {
-                // Child of root
-                graph.add_edge(root, this, ());
-            }
-        }
-    }
 }
