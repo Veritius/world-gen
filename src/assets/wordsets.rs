2309231d16a9dd4df38d4cac0b81cf1bd0926809
veritius <veritiusgaming@gmail.com>
2023-10-05 14:15:22 +1100

    Change WordSet doc comment

--- src/assets/wordsets.rs
+++ src/assets/wordsets.rs
@@ -1,7 +1,7 @@
 use bevy::reflect::{TypeUuid, TypePath};
 use serde::Deserialize;
 
-/// A set of strings.
+/// A set of words or phrases.
 #[derive(Deserialize, TypeUuid, TypePath)]
 #[uuid="77449be9-8237-4a5b-8f1b-64e06cf022ed"]
 pub struct WordSet(pub Vec<String>);
